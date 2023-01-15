6b9b81f397fe9ee14ae35f3aefa10efc08c27c39
Alexis Thierry <alexist103@gmail.com>
2023-01-15 12:58:38 +0100

    arbo

---
+++ src/gameplay/items/ArmorType/Pants.java
@@ -0,0 +1,10 @@
+package gameplay.items.ArmorType;
+
+import gameplay.items.Armor;
+
+public class Pants extends Armor {
+    public Pants(String name, int rarity, int protection) {
+        super(name, rarity, protection);
+    }
+
+}
