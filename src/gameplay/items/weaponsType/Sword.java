5a0c1c95a3537b4277dad6fd340add011c163790
Alexis Thierry <alexist103@gmail.com>
2023-01-17 17:44:35 +0100

    [IMPROVEMENT] ajout de règles dans readme

--- src/gameplay/items/weaponsType/Sword.java
+++ src/gameplay/items/weaponsType/Sword.java
@@ -5,7 +5,15 @@ import gameplay.items.Weapon;
 public class Sword extends Weapon {
     int multi_target;
 
-
+    /**
+     *
+     * @param name
+     * @param rarity
+     * @param damage
+     * @param cooldown
+     * @param range
+     * @param multi_target
+     */
     public Sword(String name, int rarity, double damage, double cooldown, double range, int multi_target) {
         super(name,rarity, damage, cooldown, range);
         this.multi_target = multi_target;
